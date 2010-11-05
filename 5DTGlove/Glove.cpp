4921d9482b6329062275bc3985be999497a46a89
Patrick Carlson <carlsonp@iastate.edu>
2010-11-05 13:42:14 -0500

    initial glove code

---
+++ 5DTGlove/Glove.cpp
@@ -0,0 +1,23 @@
+// Internal Includes
+#include "Glove.h"
+
+// Library/third-party includes
+
+// Standard includes
+
+
+
+OSG::Group getNode()
+{
+
+}
+
+void updateData()
+{
+	//_hardware -> updateData and doKalmanFilter
+}
+
+float getBend(int finger)
+{
+
+}
