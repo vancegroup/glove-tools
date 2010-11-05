3193bdbfe71c0fee7f3ec64256922f58d8e0cc96
Patrick Carlson <carlsonp@iastate.edu>
2010-11-05 14:34:15 -0500

    first try at the glove header

--- glove-tools/Glove.h
+++ glove-tools/Glove.h
@@ -16,5 +16,40 @@
 #ifndef INCLUDED_Glove_h_GUID_3988aa8d_bba7_42c5_a014_704428ba4d35
 #define INCLUDED_Glove_h_GUID_3988aa8d_bba7_42c5_a014_704428ba4d35
 
+// Internal Includes
+#include "GlovePointerTypes.h"
+
+// Library/third-party includes
+#include <osg/ref_ptr>
+#include <osg/Node>
+
+// Standard includes
+// - none
+
+namespace glove {
+	class Glove {
+		public:
+			Glove(GloveHardwarePtr hardware);
+
+			/// Fetch updated data from hardware and process it.
+			void updateData();
+
+
+			/// @name Accessors
+			/// @{
+			osg::ref_ptr<osg::Node> getNode() const;
+			float getBend(int finger) const;
+			/// @}
+		private:
+			/// @todo change to a pointer to a glovenode
+			osg::ref_ptr<osg::Node> _node;
+
+			GloveHardwarePtr _hardware;
+			std::vector<float> _bends;
+	
+	};
+
+} // end of namespace glove
+
 #endif // INCLUDED_Glove_h_GUID_3988aa8d_bba7_42c5_a014_704428ba4d35
 
