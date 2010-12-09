08e22ade903e689ac0f7b30d259c25ecc74a30c4
Ryan Pavlik <rpavlik@iastate.edu>
2010-12-09 16:34:11 -0600

    stub in calibration

---
+++ glove-tools/Calibration.cpp
@@ -0,0 +1,23 @@
+/**
+	@file Calibration.cpp
+	@brief
+
+	@date 2010
+
+	@author
+	Ryan Pavlik
+	<rpavlik@iastate.edu> and <abiryan@ryand.net>
+	http://academic.cleardefinition.com/
+	Iowa State University Virtual Reality Applications Center
+	Human-Computer Interaction Graduate Program
+*/
+
+// Internal Includes
+#include "Calibration.h"
+
+// Library/third-party includes
+// - none
+
+// Standard includes
+// - none
+
---
+++ glove-tools/Calibration.h
@@ -0,0 +1,64 @@
+/** @file Calibration.h
+	@brief
+
+	@date 2010
+
+	@author
+	Ryan Pavlik
+	<rpavlik@iastate.edu> and <abiryan@ryand.net>
+	http://academic.cleardefinition.com/
+	Iowa State University Virtual Reality Applications Center
+	Human-Computer Interaction Graduate Program
+*/
+
+
+#pragma once
+#ifndef INCLUDED_Calibration_h_GUID_7a455249_4847_42c1_ada5_8856a142caf0
+#define INCLUDED_Calibration_h_GUID_7a455249_4847_42c1_ada5_8856a142caf0
+
+// Internal Includes
+#include "Finger.h"
+
+// Library/third-party includes
+// - none
+
+// Standard includes
+#include <iostream>
+#include <iomanip>
+#include <vector>
+
+namespace glove {
+	class Calibration {
+		public:
+			Calibration();
+			~Calibration();
+			
+			void startCalibrating();
+			void stopCalibrating();
+			
+			std::vector<double> processBends(std::vector<double> const& bends);
+			
+			std::vector<double> getRanges() const;
+			std::vector<double> const& getMins() const;
+			std::vector<double> const& getMaxes() const;	
+			
+		private:
+			bool _autoCalibrating;
+			std::vector<double> _mins;
+			std::vector<double> _maxes;
+	};
+	
+	template<class Stream>
+	Stream & operator<<(Stream & s, Calibration const& c) {
+		std::vector<double> mins = c.getMins();
+		std::vector<double> maxes = c.getMaxes();
+		s << std::setw(10) << "Min" << std::setw(10) << "Max" << std::endl;
+		for (unsigned int i = 0; i < mins.size(); ++i) {
+			s << std::setw(10) << mins[i] << std::setw(10) << maxes[i] << std::endl;
+		}
+		return s;	
+	}
+}
+
+#endif // INCLUDED_Calibration_h_GUID_7a455249_4847_42c1_ada5_8856a142caf0
+
